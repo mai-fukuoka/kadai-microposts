99934d247957593cdea94bde1e68fdd7ec0ebc5c
mai.fukuoka <m.fukuoka02@gmail.com>
2021-07-27 10:22:35 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        {{--トップページのリンク--}}
+  
+    <a class="navbar-brand" href="/">Microposts</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Signup</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+         </li>
+       </ul>
+  </div>
+</nav>
+</header>
